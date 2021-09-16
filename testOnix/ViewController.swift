da981e48df64595267e22e22f2fa185babfb314d
alexsyvolap <80973788+alexsyvolap@users.noreply.github.com>
2021-09-16 21:36:43 +0300

    Initial Commit

---
+++ testOnix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testOnix
+//
+//  Created by Alex Syvolap on 16.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
