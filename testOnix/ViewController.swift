af64082176b6110e24599c7e51a8fd7a9c4ce539
alexsyvolap <80973788+alexsyvolap@users.noreply.github.com>
2021-09-17 09:32:51 +0300

    update

--- testOnix/ViewController.swift
+++ testOnix/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var goToSite: UIButton!
+    @IBOutlet weak var loader: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickGoToSiteButton(_ sender: UIButton) {
+        let siteUrl = URL(string: "http://numbersapi.com/random/year")!
+        loader.startAnimating()
+        AF.request(siteUrl).response { response in
+            if let data = response.data {
+                let json = String(data: data, encoding: String.Encoding.utf8)
+                self.resultLabel.text = json
+                self.loader.stopAnimating()
+            }
+        }
+    }
+    
 }
 
