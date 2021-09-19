448c718e61ca894d7b3f1d514ce3d4dd24186708
alexsyvolap <80973788+alexsyvolap@users.noreply.github.com>
2021-09-19 11:35:59 +0300

    dynamic label

--- testOnix/ViewController.swift
+++ testOnix/ViewController.swift
@@ -25,7 +25,15 @@ class ViewController: UIViewController {
         AF.request(siteUrl).response { response in
             if let data = response.data {
                 let json = String(data: data, encoding: String.Encoding.utf8)
+                
                 self.resultLabel.text = json
+//                self.resultLabel.numberOfLines = 0
+//                let maximumLabelSize: CGSize = CGSize(width: 280, height: 999)
+//                let expectedLabelSize: CGSize = self.resultLabel.sizeThatFits(maximumLabelSize)
+//                var newFrame: CGRect = self.resultLabel.frame
+//                newFrame.size.height = expectedLabelSize.height
+//                self.resultLabel.frame = newFrame
+                
                 self.loader.stopAnimating()
             }
         }
